88a75930c430d98a06331b356a45cc973c530c2d
MikaelFeher <mikael.feher@gmail.com>
2017-04-19 13:37:56 +0200

    Adds feature to play audio preview in the gallery

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,20 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Gallery extends Component {
-		
+	constructor(){
+		super();
+
+		this.state = {
+			isPlaying: false
+		}
+	}
+	
+
+	playPreview(previewUrl) {
+		let preview = new Audio(previewUrl);
+
+		if (this.state.isPlaying) {
+			preview.pause();
+			this.setState({isPlaying: false});		
+		} else {
+			preview.play()
+			this.setState({isPlaying: true});
+		}
+	}
+
+
 	render(){
 		console.log('gallery props', this.props);
 		const { tracks } = this.props;
+
 
 		return(
 			<div>
 				{
 					tracks.map((track, k) => {
+						console.log('track', track);
 						const trackImg = track.album.images[0].url
 						return(
 							<div 
 								key={k}
 								className="Track"
+								onClick={() => this.playPreview(track.preview_url)}
 							>
 								<img 
 									src={trackImg}
